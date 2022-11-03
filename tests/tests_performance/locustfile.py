76b1f5f2d40f40fe2b6407ea31c58b74687251e6
Marguerite Effren <pascaline_vigouroux@hotmail.com>
2022-11-03 17:52:52 +0100

    tests performance

---
+++ tests/tests_performance/locustfile.py
@@ -0,0 +1,25 @@
+from locust import HttpUser, task
+
+class ProjectPerfTest(HttpUser):
+    @task
+    def home(self):
+        self.client.get("/")
+
+    @task
+    def login(self):
+        self.client.post("/showSummary", {"email": "john@simplylift.co"})
+
+    @task
+    def book(self):
+        self.client.get("/book/Spring%20Festival/Simply%20Lift")
+
+    @task
+    def purchase(self):
+        self.client.post(
+            "/purchasePlaces",
+            {"club": "Simply Lift", "competition": "Spring Festival", "places": 3},
+        )
+
+    @task
+    def logout(self):
+        self.client.get("/logout")
